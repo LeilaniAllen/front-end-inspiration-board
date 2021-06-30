452e51297b28a39353d93498a1baaf3bfb894693
Brittany Clemons <brittanyclemons.info@gmail.com>
2021-06-30 11:09:45 -0700

    env updated

--- src/App.js
+++ src/App.js
@@ -1,97 +1,135 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import CardsList from './components/CardList';
-import NewBoardForm from './components/NewBoardForm';
-import Board from './components/Board';
-
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import CardsList from "./components/CardList";
+import NewBoardForm from "./components/NewBoardForm";
+import Board from "./components/Board";
 
 function App() {
-
   const [boardsData, setBoardsData] = useState([]);
   const [selectedBoard, setSelectedBoard] = useState({
-    title: '',
-    owner: '',
-    board_id: null
+    title: "",
+    owner: "",
+    board_id: null,
   });
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_BACKEND_URL}/boards`, {
-    }).then((response) => {
-      setBoardsData(response.data);
-    })
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/boards`, {})
+      .then((response) => {
+        setBoardsData(response.data);
+      });
   }, []);
 
-  const selectBoard = (board) => { setSelectedBoard(board) };
+  const selectBoard = (board) => {
+    setSelectedBoard(board);
+  };
 
   const boardsElements = boardsData.map((board) => {
-    return (<li>
-      <Board board={board} onBoardSelect={selectBoard}></Board>
-    </li>)
+    console.log(board.title);
+    return (
+      <li>
+        <Board board={board} onBoardSelect={selectBoard}></Board>
+      </li>
+    );
   });
 
   const createNewBoard = (newBoard) => {
-    axios.post(`${process.env.REACT_APP_BACKEND_URL}/boards`, newBoard).then((response) => {
-      console.log("Response:", response.data.board);
-      const boards = [...boardsData];
-      boards.push(response.data.board);
-      setBoardsData(boards);
-    }).catch((error) => {
-      console.log('Error:', error);
-      alert('Couldn\'t create a new board.');
-    });
-  }
-
+    axios
+      .post(`${process.env.REACT_APP_BACKEND_URL}/boards`, newBoard)
+      .then((response) => {
+        axios
+          .get(`${process.env.REACT_APP_BACKEND_URL}/boards`, {})
+          .then((response) => {
+            setBoardsData(response.data);
+          });
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
+  };
 
   const [isBoardFormVisible, setIsBoardFormVisible] = useState(true);
-  const toggleNewBoardForm = () => { setIsBoardFormVisible(!isBoardFormVisible) }
+  const toggleNewBoardForm = () => {
+    setIsBoardFormVisible(!isBoardFormVisible);
+  };
 
   const deleteAll = () => {
-    if (window.confirm('Are you really sure? Please be gentle with this demo.')) {
-      axios.delete(`${process.env.REACT_APP_BACKEND_URL}/destroy_all`).then((response) => {
-        console.log('response', response.data);
-        setBoardsData([response.data.default_board]);
-        setSelectedBoard({
-          title: '',
-          owner: '',
-          board_id: null
+    if (
+      window.confirm("Are you really sure? Please be gentle with this demo.")
+    ) {
+      axios
+        .delete(`${process.env.REACT_APP_BACKEND_URL}/destroy_all`)
+        .then((response) => {
+          console.log("response", response.data);
+          setBoardsData([response.data.default_board]);
+          setSelectedBoard({
+            title: "",
+            owner: "",
+            board_id: null,
+          });
+        })
+        .catch((error) => {
+          console.log("Error:", error);
+          alert("Something went wrong! :(");
         });
-      }).catch((error) => {
-        console.log('Error:', error);
-        alert('Something went wrong! :(');
-      });
     }
-  }
+  };
 
   return (
     <div className="page__container">
       <div className="content__container">
-        <h1 className="inspiration-board" >Inspiration Board</h1>
-        <section id="heart"></section>
-        <section className="boards__container">
+        <h1 className="inspiration-board">Inspiration Board</h1>
+        <section></section>
+        {/* This section holds the boards cards on header. */}
+        <section className="boards-container">
+          {/* Boards box element */}
           <section>
             <h2>Boards</h2>
-            <ol className="boards__list">
-              {boardsElements}
-            </ol>
+            <ol className="boards__list">{boardsElements}</ol>
           </section>
+          {/* Selected Board box element */}
           <section>
             <h2>Selected Board</h2>
-            <p>{selectedBoard.board_id ? `${selectedBoard.title} - ${selectedBoard.owner}` : 'Select a Board from the Board List!'}</p>
+            <p>
+              {selectedBoard.board_id
+                ? `${selectedBoard.title} - ${selectedBoard.owner}`
+                : "Select a Board from the Board List!"}
+            </p>
           </section>
-          <section className='new-board-form__container'>
+          {/* Create A New Board box element */}
+          <section className="new-board-form__container">
             <h2>Create a New Board</h2>
-            {isBoardFormVisible ? <NewBoardForm createNewBoard={createNewBoard}></NewBoardForm> : ''}
-            <span onClick={toggleNewBoardForm} className='new-board-form__toggle-btn'>{isBoardFormVisible ? 'Hide New Board Form' : 'Show New Board Form'}</span>
+            {isBoardFormVisible ? (
+              <NewBoardForm createNewBoard={createNewBoard}></NewBoardForm>
+            ) : (
+              ""
+            )}
+            <span
+              onClick={toggleNewBoardForm}
+              className="new-board-form__toggle-btn"
+            >
+              {isBoardFormVisible
+                ? "Hide New Board Form"
+                : "Show New Board Form"}
+            </span>
           </section>
         </section>
-        {selectedBoard.board_id ? <CardsList board={selectedBoard}></CardsList> : ''}
+        {selectedBoard.board_id ? (
+          <CardsList board={selectedBoard}></CardsList>
+        ) : (
+          ""
+        )}
       </div>
-      <footer><span>This is a demo! Please be gentle!</span> Click <span onClick={deleteAll} className="footer__delete-btn">here</span> to delete all boards and cards!</footer>
+      <footer>
+        <span>This is a demo! Please be gentle!</span> Click{" "}
+        <span onClick={deleteAll} className="footer__delete-btn">
+          here
+        </span>{" "}
+        to delete all boards and cards!
+      </footer>
     </div>
   );
 }
-
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
+
 const Card = (props) => {
     return (<div className='card-item'>
 
         <p className='card-item__message'>{props.card.message}</p>
         <ul className='card-item__controls'>
-            <li><p>{props.card.likes_count} 🦀 </p></li>
+            <li><p>{props.card.likes_count} 🧊 </p></li>
             <li><p onClick={() => props.plusOneCardItem(props.card)}>+1</p></li>
             <li><p className='card-item__delete' onClick={() => props.deleteCardItem(props.card)}>Delete</p></li>
         </ul>
